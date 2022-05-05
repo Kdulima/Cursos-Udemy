266f897f68f257cce5e578bd8c93293217909483
carloslima <carlos.eduardo@the9level.com>
2022-05-05 17:05:30 -0300

    feat(create): added class 7

---
+++ Javascript e TypeScript do básico ao avançado/Node/aula7/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+  <form action="/" method="post">
+    Nome do cliente: <input type="text" name="nome" />
+    <button>Enviar</button>
+  </form>
+  `);
+});
+
+app.post('/', (req, res) => {
+  res.send('Recebi o formulário');
+})
+
+app.get('/contato', (req, res) => {
+  res.send('Thank you for contacting us.');
+})
+
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000')
+  console.log('Example app listening on port 3000!');
+});
