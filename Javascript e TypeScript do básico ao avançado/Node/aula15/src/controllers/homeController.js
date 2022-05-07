382675b6c6fc6112d98746dc3b6640d2bb4d2113
carloslima <carlos.eduardo@the9level.com>
2022-05-06 21:02:24 -0300

    feat(create): added class 15

--- Javascript e TypeScript do básico ao avançado/Node/aula15/src/controllers/homeController.js
+++ Javascript e TypeScript do básico ao avançado/Node/aula15/src/controllers/homeController.js
@@ -1,5 +1,8 @@
 exports.paginaInicial = (req, res) => {
-  res.render('index');
+  res.render('index', {
+    titulo: 'Este será o titulo da página',
+    numeros: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  });
   return;
 } 
 
